9eca3caa104b4046cedd18b1e1c2a057792d2dd1
João Fernandes <joao.fernandesds26@gmail.com>
2021-10-21 16:04:06 -0300

    melhorias no código para pegar as variáveis do config.ini

--- chamada_eletronica/settings.py
+++ chamada_eletronica/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kn#jw#ok8!aj#n7n2bdxz9rcd2wyvp@&2*(ds&!r!$31%nak$@'
+SECRET_KEY = config.config('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
